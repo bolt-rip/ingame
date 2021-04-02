963426dcf1ac6dd471a1c24069fa2c42f1733865
Pablo Herrera <pabloherrerapalacio@gmail.com>
2021-04-02 15:47:52 +0200

    Implement forfeit after a certain amount of time

--- src/main/java/rip/bolt/ingame/ranked/ForfeitManager.java
+++ src/main/java/rip/bolt/ingame/ranked/ForfeitManager.java
@@ -3,9 +3,9 @@ package rip.bolt.ingame.ranked;
 import static tc.oc.pgm.lib.net.kyori.adventure.text.Component.text;
 
 import dev.pgm.events.Tournament;
+import dev.pgm.events.team.TournamentPlayer;
 import dev.pgm.events.team.TournamentTeamManager;
 import java.time.Duration;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -15,12 +15,12 @@ import java.util.UUID;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
-import javax.annotation.Nullable;
 import rip.bolt.ingame.config.AppData;
 import rip.bolt.ingame.utils.Messages;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchScope;
 import tc.oc.pgm.api.party.Competitor;
+import tc.oc.pgm.api.party.Party;
 import tc.oc.pgm.api.party.VictoryCondition;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.NamedTextColor;
@@ -30,113 +30,102 @@ import tc.oc.pgm.teams.Team;
 
 public class ForfeitManager {
 
-  private final Map<Competitor, ForfeitCheck> checks = new HashMap<>();
+  private static final Duration FORFEIT_DURATION = AppData.forfeitAfter();
 
-  public ForfeitManager() {}
+  private final PlayerWatcher playerWatcher;
 
-  @Nullable
-  public ForfeitCheck getChecker(Competitor team) {
-    return checks.get(team);
+  private final Map<Competitor, LeaveAnnouncer> leaves = new HashMap<>();
+  private final Map<Competitor, ForfeitPoll> forfeit = new HashMap<>();
+
+  public ForfeitManager(PlayerWatcher playerWatcher) {
+    this.playerWatcher = playerWatcher;
   }
 
-  public void clearCheckers() {
-    checks.clear();
+  public ForfeitPoll getForfeitPoll(Competitor team) {
+    return forfeit.computeIfAbsent(team, ForfeitPoll::new);
   }
 
-  public void startCountdown(Competitor team, PlayerWatcher.MatchParticipation participation) {
-    if (!AppData.allowForfeit()) return;
-    ForfeitCheck existingCheck = checks.get(team);
+  public boolean mayForfeit(Competitor team) {
+    if (!AppData.forfeitEnabled()) return false;
+    if (team.getMatch().getDuration().compareTo(FORFEIT_DURATION) >= 0) return true;
 
-    if (existingCheck != null) {
-      // Do not continue if existing check is in voting stage
-      if (existingCheck.isVotable()) {
-        existingCheck.check();
-        return;
-      }
-
-      // Do nothing if new check is longer than currently running one
-      if (existingCheck.scheduledFuture.getDelay(TimeUnit.SECONDS)
-          < participation.absentDuration().getSeconds()) return;
-
-      existingCheck.stopTimer();
-    }
-
-    checks.put(team, new ForfeitCheck(team, participation));
-  }
-
-  public void stopCountdown(
-      PlayerWatcher.MatchParticipation participation,
-      Map<UUID, PlayerWatcher.MatchParticipation> players) {
-    checks.values().stream()
-        .filter(check -> check.participation.equals(participation) && !check.isVotable())
-        .peek(ForfeitCheck::stopTimer)
-        .findFirst()
-        .map(ForfeitCheck::getTeam)
-        .ifPresent(checks::remove);
-
-    // Check if another item needs starting
-    TournamentTeamManager teamManager = Tournament.get().getTeamManager();
-    teamManager
-        .tournamentTeamPlayer(participation.uuid)
-        .map(team -> team.getPlayers().stream())
-        .orElse(Stream.empty())
-        .map(tournamentPlayer -> players.get(tournamentPlayer.getUUID()))
+    return getRegisteredPlayers(team)
+        .map(playerWatcher::getParticipation)
         .filter(Objects::nonNull)
-        .filter(PlayerWatcher.MatchParticipation::canStartCountdown)
-        .max(Comparator.comparingLong(p -> p.absentLength))
-        .ifPresent(p -> teamManager.playerTeam(p.uuid).ifPresent(t -> startCountdown(t, p)));
+        .anyMatch(PlayerWatcher.MatchParticipation::hasAbandoned);
   }
 
-  public static class ForfeitCheck {
+  public void clearPolls() {
+    leaves.clear();
+    forfeit.clear();
+  }
 
+  private Stream<UUID> getRegisteredPlayers(Competitor team) {
+    TournamentTeamManager teamManager = Tournament.get().getTeamManager();
+    return teamManager
+        .tournamentTeam(team)
+        .map(t -> t.getPlayers().stream())
+        .orElse(Stream.empty())
+        .map(TournamentPlayer::getUUID);
+  }
+
+  public void updateCountdown(Party team) {
+    if (!AppData.forfeitEnabled() || !(team instanceof Competitor)) return;
+
+    leaves.computeIfAbsent((Competitor) team, LeaveAnnouncer::new).update();
+  }
+
+  public class LeaveAnnouncer {
     private final Competitor team;
-    private final PlayerWatcher.MatchParticipation participation;
-    private final Set<UUID> voted = new HashSet<>();
+    private boolean hasCompleted;
 
     private ScheduledFuture<?> scheduledFuture;
-    private boolean votable = false;
 
-    public ForfeitCheck(Competitor team, PlayerWatcher.MatchParticipation participation) {
+    public LeaveAnnouncer(Competitor team) {
       this.team = team;
-      this.participation = participation;
-
-      startTimer();
     }
 
-    public Competitor getTeam() {
-      return team;
+    private void broadcast() {
+      if (this.hasCompleted) return;
+      this.hasCompleted = true;
+      team.sendMessage(Messages.forfeit());
+    }
+
+    private void update() {
+      if (this.hasCompleted) return;
+
+      if (scheduledFuture != null) {
+        scheduledFuture.cancel(false);
+        scheduledFuture = null;
+      }
+
+      getRegisteredPlayers(team)
+          .map(playerWatcher::getParticipation)
+          .filter(PlayerWatcher.MatchParticipation::canStartCountdown)
+          .map(PlayerWatcher.MatchParticipation::absentDuration)
+          .max(Duration::compareTo)
+          .map(PlayerWatcher.ABSENT_MAX::minus)
+          .filter(duration -> !duration.isNegative())
+          .ifPresent(
+              duration ->
+                  scheduledFuture =
+                      team.getMatch()
+                          .getExecutor(MatchScope.RUNNING)
+                          .schedule(this::broadcast, duration.toMillis(), TimeUnit.MILLISECONDS));
+    }
+  }
+
+  public static class ForfeitPoll {
+
+    private final Competitor team;
+    private final Set<UUID> voted = new HashSet<>();
+
+    public ForfeitPoll(Competitor team) {
+      this.team = team;
     }
 
     public Set<UUID> getVoted() {
       return voted;
-    }
-
-    public boolean isVotable() {
-      return votable;
-    }
-
-    private void startTimer() {
-      if (participation.absentDuration().compareTo(PlayerWatcher.ABSENT_MAX) > 0) {
-        broadcast();
-      } else {
-
-        Duration length = PlayerWatcher.ABSENT_MAX.minus(participation.absentDuration());
-        scheduledFuture =
-            team.getMatch()
-                .getExecutor(MatchScope.RUNNING)
-                .schedule(this::broadcast, length.getSeconds(), TimeUnit.SECONDS);
-      }
-    }
-
-    private void stopTimer() {
-      if (scheduledFuture.isDone() || scheduledFuture.isCancelled()) return;
-      scheduledFuture.cancel(true);
-    }
-
-    private void broadcast() {
-      votable = true;
-      team.sendMessage(Messages.forfeit());
-      check();
     }
 
     public void addVote(MatchPlayer player) {
@@ -149,10 +138,7 @@ public class ForfeitManager {
     }
 
     private boolean hasPassed() {
-      // If all but one player has voted or all of the online players
-      long votes = voted.stream().filter(uuid -> team.getPlayer(uuid) != null).count();
-
-      return votes
+      return voted.stream().filter(uuid -> team.getPlayer(uuid) != null).count()
           >= Math.min(
               team instanceof Team ? ((Team) team).getMaxPlayers() - 1 : 0,
               team.getPlayers().size());
