bf18459cc4df46d0f708d64a17440a457e6b3946
Pugzy <pugzy@mail.com>
2021-04-17 21:08:10 +0100

    Update to PGM v0.12 fixing tab entry

--- src/main/java/rip/bolt/ingame/utils/RankedTeamTabEntry.java
+++ src/main/java/rip/bolt/ingame/utils/RankedTeamTabEntry.java
@@ -2,19 +2,18 @@ package rip.bolt.ingame.utils;
 
 import dev.pgm.events.Tournament;
 import dev.pgm.events.team.TournamentTeam;
-import java.util.Arrays;
 import java.util.Optional;
 import javax.annotation.Nullable;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
 import rip.bolt.ingame.api.definitions.Team;
+import tc.oc.pgm.lib.net.kyori.adventure.text.Component;
+import tc.oc.pgm.lib.net.kyori.adventure.text.format.NamedTextColor;
+import tc.oc.pgm.lib.net.kyori.adventure.text.format.TextDecoration;
 import tc.oc.pgm.tablist.TeamTabEntry;
 import tc.oc.pgm.util.tablist.TabView;
 
 public class RankedTeamTabEntry extends TeamTabEntry {
 
-  @Nullable private BaseComponent mmrComponent;
+  @Nullable private Component mmrComponent;
   private tc.oc.pgm.teams.Team team;
 
   public RankedTeamTabEntry(tc.oc.pgm.teams.Team team) {
@@ -24,33 +23,23 @@ public class RankedTeamTabEntry extends TeamTabEntry {
   }
 
   @Override
-  public BaseComponent[] getContent(TabView view) {
-    BaseComponent[] base = super.getContent(view);
+  public Component getContent(TabView view) {
+    Component base = super.getContent(view);
 
     if (mmrComponent == null) mmrComponent = getMmrComponent();
-
-    if (mmrComponent != null) {
-      base = Arrays.copyOf(base, base.length + 1);
-      base[base.length - 1] = mmrComponent;
-    }
+    if (mmrComponent != null) return base.append(mmrComponent);
 
     return base;
   }
 
-  private BaseComponent getMmrComponent() {
+  private Component getMmrComponent() {
     Optional<TournamentTeam> tournamentTeam =
         Tournament.get().getTeamManager().tournamentTeam(team);
 
     return tournamentTeam
         .filter(t -> t instanceof Team)
         .map(t -> (Team) t)
-        .map(t -> new TextComponent("  " + t.getMmr()))
-        .map(
-            c -> {
-              c.setColor(ChatColor.GRAY);
-              c.setItalic(true);
-              return c;
-            })
+        .map(t -> Component.text("  " + t.getMmr(), NamedTextColor.GRAY, TextDecoration.ITALIC))
         .orElse(null);
   }
 }
