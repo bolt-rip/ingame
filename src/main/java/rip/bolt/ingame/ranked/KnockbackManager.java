722e7389cab35a49bfcbd82662d6a88c646b3e13
Indicado <indi88@riseup.net>
2022-06-25 12:40:52 +0200

    Run formatter

--- src/main/java/rip/bolt/ingame/ranked/KnockbackManager.java
+++ src/main/java/rip/bolt/ingame/ranked/KnockbackManager.java
@@ -1,5 +1,7 @@
 package rip.bolt.ingame.ranked;
 
+import java.util.Objects;
+import javax.annotation.Nullable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -7,27 +9,24 @@ import org.github.paperspigot.PaperSpigotConfig;
 import rip.bolt.ingame.api.definitions.BoltKnockback;
 import rip.bolt.ingame.events.BoltMatchStatusChangeEvent;
 
-import javax.annotation.Nullable;
-import java.util.Objects;
-
 public class KnockbackManager implements Listener {
-    public KnockbackManager() {}
+  public KnockbackManager() {}
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onBoltMatchStateChange(BoltMatchStatusChangeEvent event) {
-        if (Objects.equals(event.getNewStatus(), MatchStatus.CREATED)) {
-            setupKnockback(event.getBoltMatch().getSeries().getKnockback());
-        }
+  @EventHandler(priority = EventPriority.MONITOR)
+  public void onBoltMatchStateChange(BoltMatchStatusChangeEvent event) {
+    if (Objects.equals(event.getNewStatus(), MatchStatus.CREATED)) {
+      setupKnockback(event.getBoltMatch().getSeries().getKnockback());
     }
+  }
 
-    public void setupKnockback(@Nullable BoltKnockback knockback) {
-        if (knockback == null) knockback = BoltKnockback.defaults();
+  public void setupKnockback(@Nullable BoltKnockback knockback) {
+    if (knockback == null) knockback = BoltKnockback.defaults();
 
-        PaperSpigotConfig.knockbackFriction = knockback.getFriction();
-        PaperSpigotConfig.knockbackHorizontal = knockback.getHorizontal();
-        PaperSpigotConfig.knockbackVertical = knockback.getVertical();
-        PaperSpigotConfig.knockbackVerticalLimit = knockback.getVerticalLimit();
-        PaperSpigotConfig.knockbackExtraHorizontal = knockback.getExtraHorizontal();
-        PaperSpigotConfig.knockbackExtraVertical = knockback.getExtraVertical();
-    }
+    PaperSpigotConfig.knockbackFriction = knockback.getFriction();
+    PaperSpigotConfig.knockbackHorizontal = knockback.getHorizontal();
+    PaperSpigotConfig.knockbackVertical = knockback.getVertical();
+    PaperSpigotConfig.knockbackVerticalLimit = knockback.getVerticalLimit();
+    PaperSpigotConfig.knockbackExtraHorizontal = knockback.getExtraHorizontal();
+    PaperSpigotConfig.knockbackExtraVertical = knockback.getExtraVertical();
+  }
 }
